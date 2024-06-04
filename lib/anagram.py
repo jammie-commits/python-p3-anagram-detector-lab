bcf703a39c66760d7000231e711430b8d0f5213d
jammie-commits <jay.mbugua.ph@gmail.com>
2024-06-04 23:37:25 +0300

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        # Sort the characters of the initialized word
+        sorted_word = sorted(self.word)
+        
+        # Find and return the list of anagrams
+        return [word for word in words if sorted(word) == sorted_word]
+
